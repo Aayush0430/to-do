77b99255a78edaec3a02c061411772b9709a2bd5
Aayush Prajapati <aayush.prajapati0430@gmail.com>
2025-03-01 21:45:41 +0545

    changesMade

--- src/Oneline.jsx
+++ src/Oneline.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Oneline = () => {
   return (
     <div className="h-1">
-      <img src="oneline.png" className="" alt="" />
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7ac12fqebeHo76AjWlnIn-Vy5Ltzc2TUB6w&s"
+        className=""
+        alt=""
+      />
     </div>
   );
 };
